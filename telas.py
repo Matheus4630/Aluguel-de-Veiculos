c5b97a8cb3f75ac6a96db0c9632481287f603e0d
Matheus Amorim <amorim.cpm@gmail.com>
2024-09-04 00:29:10 -0300

    04/09   00:29  telas

--- telas.py
+++ telas.py
@@ -1,24 +1,24 @@
 import customtkinter
 from clientes import Clientes
 
-janela = customtkinter.CTk()
+janelaClientes = customtkinter.CTk()
 
-janela.title('Clientes')
-janela.geometry('350x300')
+janelaClientes.title('Clientes')
+janelaClientes.geometry('350x300')
 
-textoExibicao = customtkinter.CTkLabel(janela, text = 'Operações de Cliente')
+textoExibicao = customtkinter.CTkLabel(janelaClientes, text = 'Operações de Cliente')
 textoExibicao.pack(padx = 10, pady = 10)
 
-botao1 = customtkinter.CTkButton(janela, text='Criar', command=Clientes.newCliente)
+botao1 = customtkinter.CTkButton(janelaClientes, text='Criar', command=Clientes.newCliente)
 botao1.pack(padx = 10, pady = 10)
 
-botao2 = customtkinter.CTkButton(janela, text = 'Mostrar', command=Clientes.showClientes)
+botao2 = customtkinter.CTkButton(janelaClientes, text = 'Mostrar', command=Clientes.showClientes)
 botao2.pack(padx=10, pady=10)
 
-botao3 = customtkinter.CTkButton(janela, text = 'Editar', command=Clientes.editCliente)
+botao3 = customtkinter.CTkButton(janelaClientes, text = 'Editar', command=Clientes.editCliente)
 botao3.pack(padx = 10, pady=10)
 
-botao4 = customtkinter.CTkButton(janela, text = 'Deletar', command=Clientes.delCliente)
+botao4 = customtkinter.CTkButton(janelaClientes, text = 'Deletar', command=Clientes.delCliente)
 botao4.pack(padx=10, pady=10)
 
-janela.mainloop()
+janelaClientes.mainloop()
