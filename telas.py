fc19d8be10825b1dcb2f0238ba1bb956566b45e6
NaJulaa <yuno.com00@gmail.com>
2024-09-04 11:58:25 -0300

    separando por classe(telas)

--- telas.py
+++ telas.py
@@ -1,7 +1,6 @@
 import customtkinter
 from clientes import Clientes
 
-<<<<<<< Updated upstream
 janelaClientes = customtkinter.CTk()
 
 janelaClientes.title('Clientes')
@@ -23,37 +22,3 @@ botao4 = customtkinter.CTkButton(janelaClientes, text = 'Deletar', command=Clien
 botao4.pack(padx=10, pady=10)
 
 janelaClientes.mainloop()
-=======
-class TelaCliente:
-    
-    janelaC = customtkinter.CTk()
-
-    janelaC.title('Clientes')
-    janelaC.geometry('350x300')
-
-    textoExibicao = customtkinter.CTkLabel(janelaC, text = 'Operações de Cliente')
-    textoExibicao.pack(padx = 10, pady = 10)
-
-    botao1 = customtkinter.CTkButton(janelaC, text='Criar', command=Clientes.newCliente)
-    botao1.pack(padx = 10, pady = 10)
-
-    botao2 = customtkinter.CTkButton(janelaC, text = 'Mostrar', command=Clientes.showClientes)
-    botao2.pack(padx=10, pady=10)
-
-    botao3 = customtkinter.CTkButton(janelaC, text = 'Editar', command=Clientes.editCliente)
-    botao3.pack(padx = 10, pady=10)
-
-    botao4 = customtkinter.CTkButton(janelaC, text = 'Deletar', command=Clientes.delCliente)
-    botao4.pack(padx=10, pady=10)
-
-    janelaC.mainloop()
-
-
-class TelaVeiculo:
-    janelaV = customtkinter.CTk()
-
-    janelaV.title('Veiculos')
-    janelaV.geometry('400x350')
-
-
->>>>>>> Stashed changes
