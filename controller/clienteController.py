9c6b57385e1be7747ac1e9fd03f65c2ef0d6d7f1
Matheus Amorim <amorim.cpm@gmail.com>
2024-09-18 17:45:19 -0300

    18/09   16:50  CPF

--- controller/clienteController.py
+++ controller/clienteController.py
@@ -1,14 +1,20 @@
 from database.conexao import DBManager
-
+from model.validadorCPF import validate
+from telas import JanelaCPFInvalido
 
 def receberDadosC(lista):
     lista = lista
-    nome = lista [0]
-    idade = lista [1]
-    sexo = lista [2]
-    cpf = lista [3]
-    endereco = lista [4]
-    capturarEnviar(nome, idade, sexo, cpf, endereco)
+
+    if validate(lista[3]):
+        nome = lista[0]
+        idade = lista[1]
+        sexo = lista[2]
+        cpf = lista [3]
+        endereco = lista[4]
+        capturarEnviar(nome, idade, sexo, cpf, endereco)
+    else:
+        janela = JanelaCPFInvalido()
+        janela.mainloop()
 
 def capturarEnviar(nome, idade, sexo, cpf, endereco):
     DBManager().criarUsuario(nome, idade, sexo, cpf, endereco)
