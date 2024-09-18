22a196bfee4343a7b295bfda02fb77e3f768d528
NaJulaa <yuno.com00@gmail.com>
2024-09-18 02:01:55 -0300

    CRUD do banco de dados
    18/09 02:01

--- database/conexao.py
+++ database/conexao.py
@@ -1,19 +1,50 @@
 import psycopg2
 
 class DBManager:
-    def __init__(self, dbname, user, password, host):
-        self.conn = psycopg2.connect(
-            dbname=dbname,
-            user=user,
-            password=password,
-            host=host
+    def conectarBanco(self):
+        conn = psycopg2.connect(
+        host="localhost",
+        database="aluguelDeCarros",
+        user="posgres",
+        password="147885"
         )
-        self.cursor = self.conn.cursor()
+        return conn
 
-    def inserir_dados(self, coluna1, coluna2):
-        query = "INSERT INTO sua_tabela (coluna1, coluna2) VALUES (%s, %s)"
-        self.cursor.execute(query, (coluna1, coluna2))
-        self.conn.commit()
+    def criar_usuario(self,nome, idade, sexo, cpf, endereço):
+        conn = self.conectarBanco()
+        cursor = conn.cursor()
+        query = "INSERT INTO usuarios (nome, idade, sexo, cpf, endereço) VALUES (%s, %s, %s,%s,%s)"
+        cursor.execute(query, (nome, idade, sexo, cpf, endereço))
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    def listar_usuarios(self,):
+        conn = self.conectarBanco()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM usuarios")
+        usuarios = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return usuarios
+        
+    def atualizar_usuario(self,id, novo_nome, novo_email, nova_idade):
+        conn = self.conectarBanco()
+        cursor = conn.cursor()
+        query = "UPDATE usuarios SET nome = %s, email = %s, idade = %s WHERE id = %s"
+        cursor.execute(query, (novo_nome, novo_email, nova_idade, id))
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    def excluir_usuario(self,id):
+        conn = self.conectarBanco()
+        cursor = conn.cursor()
+        query = "DELETE FROM usuarios WHERE id = %s"
+        cursor.execute(query, (id,))
+        conn.commit()
+        cursor.close()
+        conn.close()
 
     def fechar_conexao(self):
         self.cursor.close()
