181712f42fbb482077c115e7ee29db5ae6c33d52
Matheus Amorim <amorim.cpm@gmail.com>
2024-09-19 01:10:05 -0300

    18/09    Telas

--- controller/veiculoController.py
+++ controller/veiculoController.py
@@ -7,12 +7,12 @@ from database.conexao import DBManager
 
 def receberDadosV(lista):
     lista = lista
-    marca = lista [0]
-    modelo = lista [1]
-    versao = lista [2]
-    motor = lista [3]
-    cor = lista [4]
-    placa = lista [5]
+    marca = lista[0]
+    modelo = lista[1]
+    versao = lista[2]
+    motor = lista[3]
+    cor = lista[4]
+    placa = lista[5]
     capturarEnviar(marca, modelo, versao, motor, cor, placa)
     
 
